a6fb3a5d47d905cc2dc919d2a29679e7d4020e13
devzdevelop <devbusinessinfluencer@gmail.com>
2024-07-19 16:03:14 -0500

    imrpoved performance.

--- src/App.js
+++ src/App.js
@@ -135,7 +135,7 @@ function List({ posts }) {
   );
 }
 
-const Archive = memo(function Archive({ show }) {
+function Archive({ show }) {
   const { onAddPost } = usePosts;
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
   const [posts] = useState(() =>
@@ -166,7 +166,7 @@ const Archive = memo(function Archive({ show }) {
       )}
     </aside>
   );
-});
+}
 
 function Footer() {
   return <footer>&copy; by The Atomic Blog ✌️</footer>;
